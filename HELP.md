88b1d045e8dda0b3ac4c831a423dd126fde9c35e
leonid <leonid.mishuck@gmail.com>
2023-07-08 17:43:05 +0200

    Added project skileton

---
+++ HELP.md
@@ -0,0 +1,12 @@
+#[Project3] Sensor web app 
+
+### Run app
+- Java 17 must be installed
+- Docker must be installed
+- Run db in docker ```docker compose up```
+- Run app ```./gradlew bootRun```
+- Run tests: ```./gradlew test```
+
+### Run postgres in docker
+- Start docker container ```docker compose up```
+- Stop docker container ```docker compose down```
---
+++ src/test/java/com/web/sensor/MainApplicationTests.java
@@ -0,0 +1,17 @@
+package com.web.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
